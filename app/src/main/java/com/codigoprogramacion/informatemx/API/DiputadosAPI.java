2440609a87a79975a4ed6e208251196fbdf925a3
xymind <jogeavi914@gmail.com>
2014-09-11 23:18:01 -0500

    initial commit

---
+++ app/src/main/java/com/codigoprogramacion/informatemx/API/DiputadosAPI.java
@@ -0,0 +1,30 @@
+package com.codigoprogramacion.informatemx.API;
+
+/*
+ * Created by xymind on 11/09/14.
+ */
+
+import retrofit.client.Response;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import rx.Observable;
+
+public interface DiputadosAPI {
+
+    @GET("/diputado/")
+    Observable<Response> getDiputados();
+
+    @GET("diputado/{id}")
+    Observable<Response> getDiputado(@Path("id") String id);
+
+    @GET("diputado/{id}/proposiciones")
+    Observable<Response> getProposiciones(@Path("id") String id);
+
+    @GET("diputado/{id}/votaciones")
+    Observable<Response> getVotaciones(@Path("id") String id);
+
+    @GET("diputado/{id}/asistencias")
+    Observable<Response> getAsistencias(@Path("id") String id);
+
+
+}
