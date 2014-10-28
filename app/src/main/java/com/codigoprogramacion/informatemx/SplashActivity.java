a6681bf4c55a97d3084272416a1f0aa92e554735
xymind <jogeavi914@gmail.com>
2014-10-27 21:18:05 -0600

    Splash y drawables

---
+++ app/src/main/java/com/codigoprogramacion/informatemx/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.codigoprogramacion.informatemx;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(
+                new Runnable(){
+
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(getBaseContext(),HomeActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                },
+            3000l
+        );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
